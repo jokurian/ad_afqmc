1976e9a3f7552e4d5e6c524eea030ce0014342ed
Ydrnan <yann.damour@hotmail.fr>
2026-01-07 16:42:06 -0800

    revert changes

--- tests/test_fp.py
+++ tests/test_fp.py
@@ -1,7 +1,5 @@
 import numpy as np
 from pyscf import gto, scf
-from ad_afqmc import config
-config.afqmc_config["use_gpu"] = True # To run on GPUs
 
 from ad_afqmc import afqmc
 
@@ -16,22 +14,23 @@ mol =  gto.M(atom ="""
 def test_rhf():
     mf = scf.RHF(mol)
     mf.kernel()
-    
+
     af = afqmc.AFQMC(mf)
     af.free_projection = True
     af.dt = 0.1
-    af.n_prop_steps = 5  # number of dt long steps in a qr block
-    af.n_qr_blocks = 6  # number of qr blocks
-    af.n_blocks = 1   # number of propagation and measurement blocks
-    af.n_ene_blocks= 10  # number of trajectories
-    af.n_walkers = 50000
+    af.n_prop_steps = 10  # number of dt long steps in a qr block
+    af.n_qr_blocks = 1  # number of qr blocks
+    af.n_blocks = 3   # number of propagation and measurement blocks
+    af.n_ene_blocks= 5  # number of trajectories
+    af.no_beta_0_fp = True
+    af.n_walkers = 10
     af.ene0 = mf.e_tot
     af.seed = 5
     e, err, sign = af.kernel()
     
-    #assert abs(e[-1]+7.624795057120907416e+01) < 1e-6
-    #assert abs(err[-1]-3.039101773566795991e-02) < 1e-8
-    #assert abs(sign[-1]-np.sign(sign[-1])*9.528328689942754393e-01) < 1e-8
+    assert abs(e[-1]+7.624795057120907416e+01) < 1e-6
+    assert abs(err[-1]-3.039101773566795991e-02) < 1e-8
+    assert abs(sign[-1]-np.sign(sign[-1])*9.528328689942754393e-01) < 1e-8
 
 def test_uhf():
     mf = scf.UHF(mol)
@@ -56,4 +55,4 @@ def test_uhf():
 
 if __name__ == "__main__":
     test_rhf()
-    #test_uhf()
+    test_uhf()
